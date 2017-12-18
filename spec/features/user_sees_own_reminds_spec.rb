3b948df28cd65315fc45cdfa946993bcb1f1b860
David Maurer <davemaurermail@gmail.com>
2017-12-18 13:55:25 -0700

    find only reminders for the current user email and add the email value to the session when creating a reminder.

--- spec/features/user_sees_own_reminds_spec.rb
+++ spec/features/user_sees_own_reminds_spec.rb
@@ -1,12 +1,11 @@
 require "rails_helper"
 
-
 feature "User sees own reminders" do
   scenario "and not other user reminders" do
-    Remind.create!(title: "Get Good", email: "other_user@user.com")
+    Remind.create!(title: "Mow the lawn", email: "other_user@user.com")
 
-    sign_in_as "user@user.com"
+    sign_in
 
-    expect(page).not_to have_css ".todos li", text: "Get Good"
+    expect(page).not_to have_css ".todos li", text: "Mow the lawn"
   end
 end
