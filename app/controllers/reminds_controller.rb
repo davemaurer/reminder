118f1f021fe8787e55d88218bbebdef514563163
David Maurer <davemaurermail@gmail.com>
2017-12-19 12:02:08 -0700

    move references of current email in the session to the application controller to make future changes happen in one place only.

--- app/controllers/reminds_controller.rb
+++ app/controllers/reminds_controller.rb
@@ -2,7 +2,7 @@ class RemindsController < ApplicationController
   before_action :authenticate
 
   def index
-    @reminds = Remind.where(email: session[:current_email])
+    @reminds = Remind.where(email: current_email)
   end
 
   def new
@@ -10,7 +10,7 @@ class RemindsController < ApplicationController
   end
 
   def create
-    Remind.create(remind_params.merge(email: session[:current_email]))
+    Remind.create(remind_params.merge(email: current_email))
     redirect_to reminds_path
   end
 
