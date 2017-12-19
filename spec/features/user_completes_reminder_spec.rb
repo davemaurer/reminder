b5da4e72e1add39e010a0c65bc14eb3a7c1dcfaa
David Maurer <davemaurermail@gmail.com>
2017-12-19 15:20:19 -0700

    add test for user completing a reminder

---
+++ spec/features/user_completes_reminder_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+feature "User completes a reminder" do
+  scenario "successfully" do
+    sign_in
+
+    click_on "Add a reminder"
+    fill_in "Title", with: "Find money"
+    click_on "Remind me"
+
+    click_on "Completed"
+
+    expect(page).to have_css ".todos li.completed", text: "Find money"
+  end
+end
